c3eabeb0c459945317204539acb940be6fee37fc
Autoforwardbotindia <138660358+Autoforwardbotindia@users.noreply.github.com>
2023-07-29 09:53:55 +0530

    Update commands.py

--- plugins/commands.py
+++ plugins/commands.py
@@ -408,7 +408,7 @@ async def start(client, message):
             )
             filetype = msg.media
             file = getattr(msg, filetype.value)
-            title = '@TeamHMT ' + ' '.join(filter(lambda x: not x.startswith('[') and not x.startswith('@'), file.file_name.split()))
+            title = '@TITANMOVIES_INDIA' + ' '.join(filter(lambda x: not x.startswith('[') and not x.startswith('@'), file.file_name.split()))
             size=get_size(file.file_size)
             f_caption = f"<code>{title}</code>"
             if CUSTOM_FILE_CAPTION:
